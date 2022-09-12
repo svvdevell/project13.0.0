f9f1afbbd102290f7405cae5bbd820e17f992b6b
svvdevell <svvdevel@gmail.com>
2022-09-13 00:14:10 +0300

    added task

--- scripts.js
+++ scripts.js
@@ -1,1 +1,36 @@
 "use strict"
+
+const API_URL = "https://jsonplaceholder.typicode.com/users";
+
+fetch(API_URL)
+    .then((response) => response.json())
+    .then((result) => {
+        let findUser = result.find((user) => user.company.name === "Johns Group");
+        console.log(findUser);
+    })
+    .catch((responseError) => {
+        if ((responseError.code = 404)) {
+            console.error("Address is not found");
+        } else {
+            console.error(responseError);
+        }
+    });
+
+const getUser = async () => {
+    try {
+        const response = await fetch(API_URL);
+        if (!response.ok) {
+            throw response;
+        }
+    const data = await response.json();
+    let requiredUser = data.find((user) => user.company.name === "Johns Group");
+    console.log(requiredUser);
+    } catch (responseError) {
+        if ((responseError.code = 404)) {
+            console.error("Address is not found");
+        } else {
+        console.error(responseError);
+        }
+    }
+};
+getUser();
